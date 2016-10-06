05a575c4cb22f2415be8715920a917cbad622901
phette23 <phette23@gmail.com>
2016-10-06 14:30:49 -0700

    subject link fix, use delegated event

--- libguides/main.js
+++ libguides/main.js
@@ -1,31 +1,32 @@
-// IIFE
-(function() {
-    // delay load so LibGuides subjects are in DOM
+// run when subjects are loaded
+$(document).on('click', '#s-lg-index-subject-btn', function(){
+    // wait for subjects to be in DOM
     setTimeout(function(){
-        // shamelessly copied from https://matthew.reidsrow.com/worknotes/184
+        // modified from https://matthew.reidsrow.com/worknotes/184
+
         // Make sure you are on the homepage
         if($('#s-lg-index-list').length > 0) {
 
         	// Loop through all the subjects
         	$('#s-lg-index-cols').find('div.panel.panel-default').each(function() {
+                // cache to decrease DOM lookups
+                var panel = $(this)
 
         		// Get the number of guides in this subject
-        		var guides = $(this).find('.badge').text();
-        		// console.log('Number of guides: ' + guides);
+        		var guides = parseInt(panel.find('.badge').text());
 
          		// If there is only one guide, change the link
-        		if(guides == 1) {
+        		if (guides === 1) {
         			// Get the link to the guide
-        			var newLink = $(this).find('.panel-collapse').find('ul.s-lg-guide-list').find('li').find('a').attr('href');
+        			var newLink = panel.find('.panel-collapse')
+                        .find('ul.s-lg-guide-list').find('li a').attr('href');
 
-        			// Remove the action elements in the heading
-        			$(this).find('.panel-heading').find('a').removeAttr('data-toggle');
-
-        			// Switch out the href
-        			$(this).find('.panel-heading').find('a').attr('href', newLink);
+        			// Remove the action elements in the heading & switch href
+        			panel.find('.panel-heading').find('a')
+                        .removeAttr('data-toggle').attr('href', newLink);
         		}
         	});
 
         }
     }, 500)
-})()
+})
