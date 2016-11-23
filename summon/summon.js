8c070278964df0a1b13b1f655e0d03469dbcb70c
phette23 <phette23@gmail.com>
2016-11-23 10:00:04 -0800

    try to fix course best bet code

--- summon/summon.js
+++ summon/summon.js
@@ -10,18 +10,30 @@ $('#linksMenu').css('background', '#777')
 // course code pattern: five capital letters preceded either by nothing or whitespace
 // followed either by nothing, whitespace, or a hyphen (e.g. PHOTO-101)
 var courseRegex = /(^|\s)[A-Z]{5}(\s|-|$)/
-if (angular.element($('#searchBox_00V')).scope().search.query.match(courseRegex)) {
-    // copied from actual best bet HTML
-    // uses ES6 template string so we have to compile with babel before minifying
-    var html = `<div class="details">
-    <span class="bg-img site-green_arrow"></span>
-    <!-- ngIf: !item.bet.link -->
-    <!-- ngIf: !!item.bet.link --><h3 ng-if="!!item.bet.link" class="ng-scope">
-      <a ng-href="https://libraries.cca.edu/content/course-reserves-request" target="_blank" class="customPrimaryLink ng-binding" ng-bind="item.bet.title" href="https://libraries.cca.edu/content/course-reserves-request">Course Reserves</a>
-    </h3><!-- end ngIf: !!item.bet.link -->
-    <div>
-      <span ng-bind-html="item.bet.description" class="ng-binding"><p>Look in the library catalog for your course reserves.</p></span>
-    </div>
-  </div>`
-    $('.bestBet').html(html)
+// run inside recursive function so we can execute once jQuery is available
+function courseBestBet() {
+    if ($ && $('form[role="search"]').scope()) {
+        // using Angular to find query
+		console.log('jQuery found');
+        if ($('form[role="search"]').scope().search.query.match(courseRegex)) {
+            console.log('query matches course regex')
+            // copied from actual best bet HTML
+            // uses ES6 template string so we have to compile with babel before minifying
+            var html = `<div class="details">
+            <span class="bg-img site-green_arrow"></span>
+            <!-- ngIf: !item.bet.link -->
+            <!-- ngIf: !!item.bet.link --><h3 ng-if="!!item.bet.link" class="ng-scope">
+              <a ng-href="https://libraries.cca.edu/content/course-reserves-request" target="_blank" class="customPrimaryLink ng-binding" ng-bind="item.bet.title" href="https://libraries.cca.edu/content/course-reserves-request">Course Reserves</a>
+            </h3><!-- end ngIf: !!item.bet.link -->
+            <div>
+              <span ng-bind-html="item.bet.description" class="ng-binding"><p>Look in the library catalog for your course reserves.</p></span>
+            </div>
+          </div>`
+            $('.bestBet').html(html)
+        }
+    } else {
+        console.log('no jQuery yet...');
+        setTimeout(courseBestBet, 300)
+    }
 }
+courseBestBet()
