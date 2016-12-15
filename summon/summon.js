49e5df6d793743e7ad57e9598b6e4d0f90d99b70
phette23 <phette23@gmail.com>
2016-12-15 10:41:45 -0800

    (working) alert stylings for 'did you mean...' box, ref #2
    restructure the way the JavaScript is initialized

--- summon/summon.js
+++ summon/summon.js
@@ -1,23 +1,15 @@
-// load CSS adjustments
-$('head').append('<link rel="stylesheet" type="text/css" href="https://libraries.cca.edu/summon/summon.min.css">')
-
-// adjust main logo size/spacing on large screens
-$('div[size="large"] .logo')
-    .attr('src', 'https://s3.amazonaws.com/libapps/accounts/21563/images/orange-logo-620w.jpg')
-
-// highlight the "did you mean" box more, combines with styles in custom CSS
-$('.didYouMean').addClass('alert alert-warning')
-
-// course reserves suggestion
-// course code pattern: five capital letters preceded either by nothing or whitespace
-// followed either by nothing, whitespace, or a hyphen (e.g. PHOTO-101)
-var courseRegex = /(^|\s)[A-Z]{5}(\s|-|$)/
-// run inside recursive function so we can execute once jQuery is available
+// provide "best bet" like functionality for any query that matches
+// a course code such as "ARCHT-101"
 function courseBestBet() {
-    if ($ && $('form[role="search"]').scope()) {
-        // using Angular to find query
-		console.log('jQuery found');
-        if ($('form[role="search"]').scope().search.query.match(courseRegex)) {
+    // course reserves suggestion
+    // course code pattern: five capital letters preceded either by nothing or whitespace
+    // followed either by nothing, whitespace, or a hyphen (e.g. PHOTO-101)
+    var courseRegex = /(^|\s)[A-Z]{5}(\s|-|$)/
+
+    $('form input[name="q"]').ready(() => {
+        console.log('form ready')
+        // @TODO doesn't work, val() isn't ready ever
+        if ($(this).text().match(courseRegex)) {
             console.log('query matches course regex')
             // copied from actual best bet HTML
             // uses ES6 template string so we have to compile with babel before minifying
@@ -33,9 +25,31 @@ function courseBestBet() {
           </div>`
             $('.bestBet').html(html)
         }
+    })
+}
+
+function init() {
+    $(document).ready(() => {
+        // load CSS adjustments
+        $('head').append('<link rel="stylesheet" type="text/css" href="https://libraries.cca.edu/summon/summon.min.css">')
+
+        // adjust main logo size/spacing on large screens
+        $('div[size="large"] .logo')
+            .attr('src', 'https://s3.amazonaws.com/libapps/accounts/21563/images/orange-logo-620w.jpg')
+
+        // course reserve "best bet" for course-like queries
+        courseBestBet()
+    })
+}
+
+// our scripts often load before jQuery unfortunately & we need that
+// @TODO is code actually less complex as vanilla JS without jQuery?
+(function initWhenjQueryReady() {
+    if (jQuery) {
+        console.log('jQuery found, running init()')
+        init()
     } else {
         console.log('no jQuery yet...');
-        setTimeout(courseBestBet, 300)
+        setTimeout(initWhenjQueryReady, 300)
     }
-}
-courseBestBet()
+})()
