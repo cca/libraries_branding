cd1b9eb70007b033777216ecd2cc0d13c6259a1e
phette23 <phette23@gmail.com>
2017-09-07 10:26:03 -0700

    summon - simplify JS, vanilla & not jQuery

--- summon/summon.js
+++ summon/summon.js
@@ -1,22 +1,14 @@
-function init() {
-    $(document).ready(() => {
+(function(d) {
+    d.addEventListener('DOMContentLoaded', () => {
+        let css = d.createElement('link')
+        css.setAttribute('rel', 'stylesheet')
+        css.setAttribute('type', 'text/css')
+        css.href = 'https://libraries.cca.edu/static/summon/summon.min.css'
         // load CSS adjustments
-        $('head').append('<link rel="stylesheet" type="text/css" href="https://libraries.cca.edu/static/summon/summon.min.css">')
+        d.querySelector('head').append(css)
 
         // adjust main logo size/spacing on large screens
-        $('div[size="large"] .logo')
-            .attr('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
+        d.querySelector('div[size="large"] .logo')
+            .setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
     })
-}
-
-// our scripts often load before jQuery unfortunately & we need that
-// @TODO is code actually less complex as vanilla JS without jQuery?
-(function initWhenjQueryReady() {
-    if (jQuery) {
-        console.log('jQuery found, running init()')
-        init()
-    } else {
-        console.log('no jQuery yet...');
-        setTimeout(initWhenjQueryReady, 300)
-    }
-})()
+})(document)
