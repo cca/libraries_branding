711106f2754887fd2669ccf5bcf5ed9c5fba7e67
phette23 <phette23@gmail.com>
2017-11-08 12:21:53 -0800

    summon - track bX Recommender clicks

--- summon/summon.js
+++ summon/summon.js
@@ -10,5 +10,14 @@
         // adjust main logo size/spacing on large screens
         d.querySelector('div[size="large"] .logo')
             .setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
+
+        // event tracking for bX recommender
+        let query = new URLSearchParams(location.hash).get('q')
+        // track every click on results area but only send events for bx links
+        d.querySelector('#results').addEventListener('click', (ev) => {
+            if (ev.target.parentNode.classList.contains('bxRecommenderLink')) {
+                _gaq.push(['_trackEvent', 'bX Recommender', 'click', query])
+            }
+        })
     }, 500)
 })(document)
