0de9aafcd3ebee58f4d819cf77c6c64a6a79a15d
phette23 <phette23@gmail.com>
2017-12-11 15:35:27 -0800

    libguides - first draft of analytics for a-z list

--- libguides/custom-analytics.js
+++ libguides/custom-analytics.js
@@ -7,9 +7,46 @@ ga('create', 'UA-18459158-3', 'auto');
 ga('send', 'pageview');
 
 $('a[href^=http]').on('click', (ev) => {
-    var href = this.href;
+    let href = ev.target.href;
 
+    // note: doesn't work all our DB aliases, which are on LibGuides domain
     if (!href.match(location.origin)) {
         ga('send', 'event', 'Outbound links', 'Click', href);
     }
 });
+
+// special event tracking for A-Z list
+if (location.pathname.match('/az.php')) {
+    $('.s-lg-az-result-title a').on('click', (ev) => {
+        // 1 = first DB listed, closer to 0 is lowest along the list
+        let num_dbs = $('.s-lg-az-result-title').length
+        let ranking =   (num_dbs - $('.s-lg-az-result-title a').index(ev.target)) / num_dbs
+        ga('send', 'event', 'TEST A-Z List', 'DB Click', ev.target.href, ranking);
+    })
+
+    // alphabet buttons
+    $('.s-lg-az-header.btn').on('click', (ev) => {
+        ga('send', 'event', 'TEST A-Z List', 'Letter', ev.target.textContent);
+    })
+
+    // filters
+    $('#s-lg-az-filter-cols').on('blur change', 'select', (ev) => {
+        // use <select> ID to see which filter it is
+        let filter = $(ev.target).parent().find('label').text()
+        ga('send', 'event', 'TEST A-Z List', 'Filter', filter);
+    })
+
+    // clear all filters button
+    $('#s-lg-az-reset').on('click', (ev) => {
+        ga('send', 'event', 'TEST A-Z List', 'Filter', 'Clear Filters/Browse All Databases');
+    })
+
+    // popular & new/trial DBs
+    $('#s-lg-az-popular').on('click', '.s-lg-az-result-title a', (ev) => {
+        ga('send', 'event', 'TEST A-Z List', 'Popular DB Click', ev.target.href);
+    })
+    $('#s-lg-az-trials-div').on('click', '.s-lg-az-result-title a', (ev) => {
+        ga('send', 'event', 'TEST A-Z List', 'New / Trial DB Click', ev.target.href);
+    })
+
+}
