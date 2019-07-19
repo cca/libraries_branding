a77ba3a31b95a6d2733714c46ae0c5c02de29f8f
phette23 <phette23@gmail.com>
2019-07-19 16:09:25 -0700

    general cleanup
    move Summon SASS to subdir
    add setup section to readme
    delete a few unneeded files
    add trash-cli dep

--- summon/ingest-urls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# deleted bibs report
-open 'https://library-staff.cca.edu/cgi-bin/koha/reports/guided_reports.pl?reports=152&phase=Run%20this%20report'
-# updated bibs report
-open 'https://library-staff.cca.edu/cgi-bin/koha/reports/guided_reports.pl?reports=151&phase=Run+this+report'
-# export data tool
-open 'https://library-staff.cca.edu/cgi-bin/koha/tools/export.pl'
-
