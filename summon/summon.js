1841e3e4a43a56029a11beefe694f46b55e08036
phette23 <phette23@gmail.com>
2019-08-12 14:14:08 -0700

    summon - add Tabatha Farney's G Tags
    see https://github.com/tabathafarney/GoogleTagManager-Summon

--- summon/summon.js
+++ summon/summon.js
@@ -1,23 +1,34 @@
-(function(d) {
-    setTimeout(() => {
-        let css = d.createElement('link')
-        css.setAttribute('rel', 'stylesheet')
-        css.setAttribute('type', 'text/css')
-        css.href = 'https://libraries.cca.edu/static/summon/summon.min.css'
-        // load CSS adjustments
-        d.querySelector('head').append(css)
+// run on a delay...#results load async not there when page loads
+setTimeout(main, 500)
 
-        // adjust main logo size/spacing on large screens
-        d.querySelector('div[size="large"] .logo')
-            .setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
+function main() {
+    ccaCustomizations(document)
+    addGTM()
+}
 
-        // event tracking for bX recommender
-        let query = new URLSearchParams(location.hash).get('q')
-        // track every click on results area but only send events for bx links
-        d.querySelector('#results').addEventListener('click', (ev) => {
-            if (ev.target.parentNode.classList.contains('bxRecommenderLink')) {
-                _gaq.push(['_trackEvent', 'bX Recommender', 'click', query])
-            }
-        })
-    }, 500)
-})(document)
+function ccaCustomizations(d) {
+    let css = d.createElement('link')
+    css.setAttribute('rel', 'stylesheet')
+    css.setAttribute('type', 'text/css')
+    css.href = 'https://libraries.cca.edu/static/summon/summon.min.css'
+    // load CSS adjustments
+    d.querySelector('head').append(css)
+
+    // adjust main logo size/spacing on large screens
+    d.querySelector('div[size="large"] .logo')
+        .setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
+
+    // event tracking for bX recommender
+    let query = new URLSearchParams(location.hash).get('q')
+    // track every click on results area but only send events for bx links
+    d.querySelector('#results').addEventListener('click', (ev) => {
+        if (ev.target.parentNode.classList.contains('bxRecommenderLink')) {
+            _gaq.push(['_trackEvent', 'bX Recommender', 'click', query])
+        }
+    })
+}
+
+function addGTM() {
+    // https://github.com/tabathafarney/GoogleTagManager-Summon/blob/master/add-gtm-summon.js
+    $("body").prepend("<script>var vPagePath = window.location.href;dataLayer = [{'virtualURL': vPagePath}];</script><noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-TKFJGG7' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TKFJGG7');</script>");
+}
