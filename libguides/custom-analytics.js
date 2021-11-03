bb2ccc58ab48f967ae81e4704c94c4f3b7c360e8
Eric Phetteplace <phette23@gmail.com>
2021-11-03 12:03:23 -0700

    switch to eslint

--- libguides/custom-analytics.js
+++ libguides/custom-analytics.js
@@ -1,6 +1,8 @@
 // requires gtag.js (see `npm run libguides-analytics` command)
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+window.dataLayer = window.dataLayer || []
+function gtag(){
+    dataLayer.push(arguments)
+}
 gtag('js', new Date())
 gtag('config', 'UA-18459158-3', {
     'allow_ad_personalization_signals': false,
@@ -18,46 +20,46 @@ let gae = (cat, action, label, value) => {
     })
 }
 
-$('a[href^=http]').on('click', (ev) => {
-    let href = ev.target.href;
+$('a[href^=http]').on('click', ev => {
+    let href = ev.target.href
 
     // note: doesn't work for our DB aliases, which are on LibGuides domain
     if (!href.match(location.origin)) {
-        gae('Outbound links', 'Click', href);
+        gae('Outbound links', 'Click', href)
     }
-});
+})
 
 // special event tracking for A-Z list
 if (location.pathname.match('/az.php')) {
-    $('#s-lg-az-content').on('click', '.s-lg-az-result-title a', (ev) => {
+    $('#s-lg-az-content').on('click', '.s-lg-az-result-title a', ev => {
         // 1 = first DB listed, closer to 0 is lowest along the list
         let num_dbs = $('.s-lg-az-result-title a').length
         let ranking =   (num_dbs - $('.s-lg-az-result-title a').index(ev.target)) * 100 / num_dbs
-        gae('A-Z List', 'DB Click', ev.target.href, ranking);
+        gae('A-Z List', 'DB Click', ev.target.href, ranking)
     })
 
     // alphabet buttons
-    $('#s-lg-az-index').on('click', (ev) => {
-        gae('A-Z List', 'Letter', ev.target.textContent);
+    $('#s-lg-az-index').on('click', ev => {
+        gae('A-Z List', 'Letter', ev.target.textContent)
     })
 
     // filters
-    $('#s-lg-az-filter-cols').on('blur change', 'select', (ev) => {
+    $('#s-lg-az-filter-cols').on('blur change', 'select', ev => {
         // use <select> ID to see which filter it is
         let filter = $(ev.target).parent().find('label').text()
-        gae('A-Z List', 'Filter', filter);
+        gae('A-Z List', 'Filter', filter)
     })
 
     // clear all filters button
-    $('#s-lg-az-reset').on('click', (ev) => {
-        gae('A-Z List', 'Filter', 'Clear Filters/Browse All Databases');
+    $('#s-lg-az-reset').on('click', () => {
+        gae('A-Z List', 'Filter', 'Clear Filters/Browse All Databases')
     })
 
     // popular & new/trial DBs
-    $('#s-lg-az-popular').on('click', '.s-lg-az-result-title a', (ev) => {
-        gae('A-Z List', 'Popular DB Click', ev.target.href);
+    $('#s-lg-az-popular').on('click', '.s-lg-az-result-title a', ev => {
+        gae('A-Z List', 'Popular DB Click', ev.target.href)
     })
-    $('#s-lg-az-trials-div').on('click', '.s-lg-az-result-title a', (ev) => {
-        gae('A-Z List', 'New / Trial DB Click', ev.target.href);
+    $('#s-lg-az-trials-div').on('click', '.s-lg-az-result-title a', ev => {
+        gae('A-Z List', 'New / Trial DB Click', ev.target.href)
     })
 }
--- libguides/main.js
+++ libguides/main.js
@@ -1,31 +1,30 @@
 // run when subjects are loaded
-$(document).on('click', '#s-lg-index-subject-btn', function(){
+$(document).on('click', '#s-lg-index-subject-btn', () => {
     // wait for subjects to be in DOM
-    setTimeout(function(){
+    setTimeout(() => {
         // modified from https://matthew.reidsrow.com/worknotes/184
 
         // Make sure you are on the homepage
         if ($('#s-lg-index-list').length > 0) {
 
-        	// Loop through all the subjects
-        	$('#s-lg-index-cols').find('div.panel.panel-default').each(function() {
+            // Loop through all the subjects
+            $('#s-lg-index-cols').find('div.panel.panel-default').each(function() {
                 // cache to decrease DOM lookups
-                var panel = $(this)
+                let panel = $(this)
+                // Get the number of guides in this subject
+                let guides = parseInt(panel.find('.badge').text())
 
-        		// Get the number of guides in this subject
-        		var guides = parseInt(panel.find('.badge').text());
+                 // If there is only one guide, change the link
+                if (guides === 1) {
+                    // Get the link to the guide
+                    let newLink = panel.find('.panel-collapse')
+                        .find('ul.s-lg-guide-list').find('li a').attr('href')
 
-         		// If there is only one guide, change the link
-        		if (guides === 1) {
-        			// Get the link to the guide
-        			var newLink = panel.find('.panel-collapse')
-                        .find('ul.s-lg-guide-list').find('li a').attr('href');
-
-        			// Remove the action elements in the heading & switch href
-        			panel.find('.panel-heading').find('a')
-                        .removeAttr('data-toggle').attr('href', newLink);
-        		}
-        	});
+                    // Remove the action elements in the heading & switch href
+                    panel.find('.panel-heading').find('a')
+                        .removeAttr('data-toggle').attr('href', newLink)
+                }
+            })
 
         }
     }, 500)
